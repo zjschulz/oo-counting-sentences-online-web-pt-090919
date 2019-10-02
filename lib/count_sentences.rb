fcc411ef65f61bd9668c3810bb1468bc478c2845
Zachary Schulz <zjschulz@gmail.com>
2019-10-02 23:27:55 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -28,7 +28,7 @@ class String
 
   def count_sentences
   delimiters = [".","!","?"]
-  self.split(Regexp.union(delimiters)).reject! { |s| s.nil? || s.strip.empty? }.count
+  self.split(Regexp.union(delimiters)).count
   end
   
 end
