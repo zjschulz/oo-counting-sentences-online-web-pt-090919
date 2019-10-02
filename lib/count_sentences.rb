d1072a39fef476e63f5b8420c80c403902f7a048
Zachary Schulz <zjschulz@gmail.com>
2019-10-02 23:28:33 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -28,7 +28,7 @@ class String
 
   def count_sentences
   delimiters = [".","!","?"]
-  self.split(Regexp.union(delimiters)).delete()count
+  self.split(Regexp.union(delimiters)).delete().count
   end
   
 end
